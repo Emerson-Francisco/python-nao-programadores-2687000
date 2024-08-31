327e81f607d25af3fe3dd4a542af311badd52084
Emerson-Francisco <emersonfrancisco.eng@gmail.com>
2024-08-31 16:27:10 +0000

    variaveis

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,24 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
+nome = 'Emerson'
+
+profissao="engenheiro"
+
+objetivo = '''
+Aprender python
+para mudar'
+de profisao
+'''
 
 # Declare variáveis de cada tipo numérico
+idade = 49
 
+salario = 7777.70
+
+complexo = 2+1j
 
 # Declare uma variável do tipo booleano
+verdadeiro = True
+
+falso = False
